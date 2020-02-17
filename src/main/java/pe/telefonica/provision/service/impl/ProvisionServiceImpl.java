ef5b57e9375cb7346fcad9148c8e0f645ca46ecf
afalva <afalva@indracompany.com>
2020-02-17 15:54:15 -0500

    Correccion error descripcion RESCHEDULED

--- src/main/java/pe/telefonica/provision/service/impl/ProvisionServiceImpl.java
+++ src/main/java/pe/telefonica/provision/service/impl/ProvisionServiceImpl.java
@@ -1340,7 +1340,8 @@ public class ProvisionServiceImpl implements ProvisionService {
 			provision.setGenericSpeech(speech);
 			provision.setDescriptionStatus(description);
 
-			updated = provisionRepository.updateTrackingStatus(optionalProvision.get(), logStatus, comesFromSchedule);
+			updated = provisionRepository.updateTrackingStatus(optionalProvision.get(), logStatus, description, speech,
+					comesFromSchedule);
 			log.info(ProvisionServiceImpl.class.getCanonicalName() + " - updateTrackingStatus: updated = " + updated);
 		}
 

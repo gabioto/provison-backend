c98af6a31ff3e3bcf8e2d5e64fa659bd609dd4c0
Marcos Zorrilla <marc.zg21@gmail.com>
2020-05-08 18:12:14 -0500

    update mi historia 605

--- src/main/java/pe/telefonica/provision/service/impl/ProvisionServiceImpl.java
+++ src/main/java/pe/telefonica/provision/service/impl/ProvisionServiceImpl.java
@@ -563,6 +563,7 @@ public class ProvisionServiceImpl implements ProvisionService {
 				provision.setFrontSpeech(paid != null ? paid.getFront() : Status.PAGADO.getFrontSpeech());
 				provision.setActiveStatus(Status.PAGADO.getStatusName().toLowerCase());
 				provision.setStatusToa(Status.PAGADO.getStatusName().toLowerCase());
+				provision.setSendNotify(false);
 			}
 
 			listLog.add(statusLogCurrent);
@@ -780,6 +781,7 @@ public class ProvisionServiceImpl implements ProvisionService {
 				provisionx.setGenericSpeech(paid != null ? paid.getGenericSpeech() : Status.PAGADO.getGenericSpeech());
 				provisionx.setFrontSpeech(paid != null ? paid.getFront() : Status.PAGADO.getFrontSpeech());
 				status = Status.PAGADO.getStatusName().toLowerCase();
+				provisionx.setSendNotify(false);
 			}
 
 			if (provisionx.getDummyStPsiCode() != null && provisionx.getIsUpdatedummyStPsiCode() != true) {
@@ -845,7 +847,7 @@ public class ProvisionServiceImpl implements ProvisionService {
 		} else {
 
 			Provision provision = fillProvisionInsert(request);
-			provision = evaluateProvisionComponents(provision);
+			provision = evaluateProvisionComponents(provision);			
 			provisionRepository.insertProvision(provision);
 			return true;
 		}

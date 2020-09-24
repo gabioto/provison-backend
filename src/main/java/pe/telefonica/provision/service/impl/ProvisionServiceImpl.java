dfc9bde17b266fd44f9149a6fb03743ba53a38ba
wilson lazo <wjlazo@indracompany.com>
2020-09-24 13:39:39 -0500

    fixed remove validate activity type by controller

--- src/main/java/pe/telefonica/provision/service/impl/ProvisionServiceImpl.java
+++ src/main/java/pe/telefonica/provision/service/impl/ProvisionServiceImpl.java
@@ -2035,7 +2035,7 @@ public class ProvisionServiceImpl implements ProvisionService {
 	}
 
 	@Override
-	public boolean provisionUpdateFromTOA(UpdateFromToaRequest request, String xaRequest, String xaRequirementNumber)
+	public boolean provisionUpdateFromTOA(UpdateFromToaRequest request)
 			throws Exception {
 		
 		

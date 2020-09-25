621faa87e00f66d01d3255318d257ec760dc5b09
Augusto Alva Campos <afalva@indracompany.com>
2020-09-25 12:38:31 -0500

    SAQR-994 - Logs en reintentos

--- src/main/java/pe/telefonica/provision/service/impl/ProvisionServiceImpl.java
+++ src/main/java/pe/telefonica/provision/service/impl/ProvisionServiceImpl.java
@@ -1590,87 +1590,99 @@ public class ProvisionServiceImpl implements ProvisionService {
 		Provision provision = provisionRepository.getProvisionByXaIdSt(request.getPsiCode());
 
 		PSIUpdateClientRequest psiRequest = new PSIUpdateClientRequest();
+		int count = 0;
+		int maxTries = 2;
 
-		try {
-			if (provision != null) {
-				List<ContactRequest> listContact = request.getContacts();
-				List<Contacts> contactsList = new ArrayList<>();
+		while (true) {
+			try {
+				log.info("Attempt times: " + (count + 1));
 
-				if (request.isHolderWillReceive()) {
-					ContactRequest contactRequest = new ContactRequest();
-					contactRequest.setFullName(provision.getCustomer().getName());
-					contactRequest.setPhoneNumber((provision.getCustomer().getPhoneNumber() != null
-							&& !provision.getCustomer().getPhoneNumber().isEmpty())
-									? Integer.valueOf(provision.getCustomer().getPhoneNumber())
-									: 0);
-					request.getContacts().clear();
-					request.getContacts().add(contactRequest);
-				}
+				if (provision != null) {
+					List<ContactRequest> listContact = request.getContacts();
+					List<Contacts> contactsList = new ArrayList<>();
 
-				for (int a = 0; a < request.getContacts().size(); a++) {
-					Contacts contacts = new Contacts();
-					contacts.setFullName(listContact.get(a).getFullName());
-					contacts.setPhoneNumber(listContact.get(a).getPhoneNumber().toString());
-					boolean isMovistar = restPSI.getCarrier(listContact.get(a).getPhoneNumber().toString());
-					contacts.setCarrier(isMovistar);
-					contactsList.add(contacts);
-
-					if (a == 0) {
-						psiRequest.getBodyUpdateClient().setNombre_completo(listContact.get(a).getFullName());
-						psiRequest.getBodyUpdateClient().setTelefono1(listContact.get(a).getPhoneNumber().toString());
+					if (request.isHolderWillReceive()) {
+						ContactRequest contactRequest = new ContactRequest();
+						contactRequest.setFullName(provision.getCustomer().getName());
+						contactRequest.setPhoneNumber((provision.getCustomer().getPhoneNumber() != null
+								&& !provision.getCustomer().getPhoneNumber().isEmpty())
+										? Integer.valueOf(provision.getCustomer().getPhoneNumber())
+										: 0);
+						request.getContacts().clear();
+						request.getContacts().add(contactRequest);
 					}
 
-					if (a == 1) {
-						psiRequest.getBodyUpdateClient().setNombre_completo2(listContact.get(a).getFullName());
-						psiRequest.getBodyUpdateClient().setTelefono2(listContact.get(a).getPhoneNumber().toString());
+					for (int a = 0; a < request.getContacts().size(); a++) {
+						Contacts contacts = new Contacts();
+						contacts.setFullName(listContact.get(a).getFullName());
+						contacts.setPhoneNumber(listContact.get(a).getPhoneNumber().toString());
+						boolean isMovistar = restPSI.getCarrier(listContact.get(a).getPhoneNumber().toString());
+						contacts.setCarrier(isMovistar);
+						contactsList.add(contacts);
+
+						if (a == 0) {
+							psiRequest.getBodyUpdateClient().setNombre_completo(listContact.get(a).getFullName());
+							psiRequest.getBodyUpdateClient()
+									.setTelefono1(listContact.get(a).getPhoneNumber().toString());
+						}
+
+						if (a == 1) {
+							psiRequest.getBodyUpdateClient().setNombre_completo2(listContact.get(a).getFullName());
+							psiRequest.getBodyUpdateClient()
+									.setTelefono2(listContact.get(a).getPhoneNumber().toString());
+						}
+
+						if (a == 2) {
+							psiRequest.getBodyUpdateClient().setNombre_completo3(listContact.get(a).getFullName());
+							psiRequest.getBodyUpdateClient()
+									.setTelefono3(listContact.get(a).getPhoneNumber().toString());
+						}
+
+						if (a == 3) {
+							psiRequest.getBodyUpdateClient().setNombre_completo4(listContact.get(a).getFullName());
+							psiRequest.getBodyUpdateClient()
+									.setTelefono4(listContact.get(a).getPhoneNumber().toString());
+						}
+
 					}
 
-					if (a == 2) {
-						psiRequest.getBodyUpdateClient().setNombre_completo3(listContact.get(a).getFullName());
-						psiRequest.getBodyUpdateClient().setTelefono3(listContact.get(a).getPhoneNumber().toString());
+					psiRequest.getBodyUpdateClient().setSolicitud(provision.getXaIdSt());
+					psiRequest.getBodyUpdateClient().setCorreo(
+							provision.getCustomer().getMail() != null ? provision.getCustomer().getMail() : "");
+
+					System.out.println(provision.getCustomer().getMail());
+
+					boolean updatedPsi = restPSI.updatePSIClient(psiRequest);
+
+					if (updatedPsi) {
+						Update update = new Update();
+						update.set("contacts", request.isHolderWillReceive() ? null : contactsList);
+						provisionRepository.updateProvision(provision, update);
+
+						if (provision.getContacts() != null) {
+							provision.getContacts().clear();
+						}
+
+						provision.setContacts(request.isHolderWillReceive() ? null : contactsList);
+
+						// if (!request.isHolderWillReceive()) {
+						// sendInfoUpdateSMS(provision);
+						// sendContactInfoChangedMail(provision);
+						// }
+					} else {
+						throw new Exception();
 					}
 
-					if (a == 3) {
-						psiRequest.getBodyUpdateClient().setNombre_completo4(listContact.get(a).getFullName());
-						psiRequest.getBodyUpdateClient().setTelefono4(listContact.get(a).getPhoneNumber().toString());
-					}
+					return provision;
 
-				}
-
-				psiRequest.getBodyUpdateClient().setSolicitud(provision.getXaIdSt());
-				psiRequest.getBodyUpdateClient()
-						.setCorreo(provision.getCustomer().getMail() != null ? provision.getCustomer().getMail() : "");
-
-				System.out.println(provision.getCustomer().getMail());
-
-				boolean updatedPsi = restPSI.updatePSIClient(psiRequest);
-
-				if (updatedPsi) {
-					Update update = new Update();
-					update.set("contacts", request.isHolderWillReceive() ? null : contactsList);
-					provisionRepository.updateProvision(provision, update);
-
-					if (provision.getContacts() != null) {
-						provision.getContacts().clear();
-					}
-
-					provision.setContacts(request.isHolderWillReceive() ? null : contactsList);
-
-					// if (!request.isHolderWillReceive()) {
-					// sendInfoUpdateSMS(provision);
-					// sendContactInfoChangedMail(provision);
-					// }
 				} else {
-					throw new Exception();
+					return null;
 				}
-
-				return provision;
-
-			} else {
-				return null;
+			} catch (Exception e) {
+				if (++count == maxTries) {
+					throw e;
+				}
 			}
-		} catch (Exception e) {
-			throw e;
 		}
 	}
 
@@ -2351,10 +2363,10 @@ public class ProvisionServiceImpl implements ProvisionService {
 
 						int count = 0;
 						int maxTries = 2;
-						
+
 						while (true) {
 							log.info("Simpli Attempt #" + count);
-							
+
 							String urlSimpli = simpliConnectApi.getUrlTraking(simpliRequest);
 
 							if (urlSimpli != null) {

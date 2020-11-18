383d51b41129e753c4b48a6e4d9c961fe5533b25
Augusto Alva Campos <afalva@indracompany.com>
2020-11-18 12:39:18 -0500

    prueba contructor OrderRequest

--- src/main/java/pe/telefonica/provision/controller/request/order/OrderRequest.java
+++ src/main/java/pe/telefonica/provision/controller/request/order/OrderRequest.java
@@ -3,14 +3,10 @@ package pe.telefonica.provision.controller.request.order;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import pe.telefonica.provision.model.order.Order;
 import pe.telefonica.provision.util.DateUtil;
 
-@Getter
-@Setter
 @Data
 @NoArgsConstructor
 @AllArgsConstructor

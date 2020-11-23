0e2d1310a73231df1af8f1c68b089ea4f9dea59a
Augusto Alva Campos <afalva@indracompany.com>
2020-11-23 17:03:59 -0500

    Logs Request

--- src/main/java/pe/telefonica/provision/controller/OrderController.java
+++ src/main/java/pe/telefonica/provision/controller/OrderController.java
@@ -2,6 +2,8 @@ package pe.telefonica.provision.controller;
 
 import javax.validation.Valid;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 import pe.telefonica.provision.controller.request.order.OrderRequest;
 import pe.telefonica.provision.service.OrderService;
 import pe.telefonica.provision.service.RetreiveOrderService;
@@ -28,6 +32,8 @@ import pe.telefonica.provision.service.RetreiveOrderService;
 
 public class OrderController {
 
+	private static final Log log = LogFactory.getLog(OrderController.class);
+	
 	@Autowired
 	private OrderService orderService;
 
@@ -38,6 +44,7 @@ public class OrderController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Object> createOrder(@Valid @RequestBody OrderRequest request) throws Exception {
+		log.info(new Gson().toJson(request));
 		return orderService.createOrder(request);
 	}
 

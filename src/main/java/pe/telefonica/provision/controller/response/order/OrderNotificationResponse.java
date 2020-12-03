3989aa68af0f35976ce4d564c775d61b2ef644a4
Augusto Alva Campos <afalva@indracompany.com>
2020-12-03 16:53:21 -0500

    Validaciones en response notify orders

--- src/main/java/pe/telefonica/provision/controller/response/order/OrderNotificationResponse.java
+++ src/main/java/pe/telefonica/provision/controller/response/order/OrderNotificationResponse.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pe.telefonica.provision.model.order.Order;
-import pe.telefonica.provision.util.DateUtil;
 
 @Getter
 @Setter
@@ -16,85 +15,14 @@ import pe.telefonica.provision.util.DateUtil;
 @NoArgsConstructor
 public class OrderNotificationResponse {
 
-	private String code;
-
-	private String serviceCode;
-	
-	private String phone;
-
-	private String documentType;
-
-	private String documentNumber;
-
-	private String registerDate;
-
-	private String execSuspDate;
-
-	private String execRecoxDate;
-
-	private String note1;
-
-	private String application;
-
-	private String commercialOp;
-
 	private String contactPhone;
-	
-	private String contactCellphone;
 
 	private String contactMail;
 
-	private String errorCode;
-
-	private String errorDescription;
-
-	private String receptionDate;
-
-	private String status;
-
-	private String atisOrder;
-
-	private String cmsRequest;
-
-	private String statusOrderCode;
-
-	private String statusOrderDescription;
-
-	private String registerOrderDate;
-
-	private String releaseOrderDate;
-
-	private String note2;
-
-	private String idResult;
-
 	public OrderNotificationResponse fromOrder(Order order) {
-		code = order.getCode();
-		serviceCode = order.getServiceCode();
-		phone = order.getPhone();
-		documentType = order.getDocumentType();
-		documentNumber = order.getDocumentNumber();
-		registerDate = DateUtil.localDateTimeToString(order.getRegisterDate());
-		execSuspDate = DateUtil.localDateTimeToString(order.getExecSuspDate());
-		execRecoxDate = DateUtil.localDateTimeToString(order.getExecRecoxDate());
-		note1 = order.getNote1();
-		application = order.getApplication();
-		commercialOp = order.getCommercialOp();
-		contactPhone = order.getContactPhone();
-		contactCellphone = order.getContactCellphone();
+
+		contactPhone = order.getContactCellphone() != null ? order.getContactCellphone() : order.getContactPhone();
 		contactMail = order.getContactMail();
-		errorCode = order.getErrorCode();
-		errorDescription = order.getErrorDescription();
-		receptionDate = DateUtil.localDateTimeToString(order.getReceptionDate());
-		status = order.getStatus();
-		atisOrder = order.getAtisOrder();
-		cmsRequest = order.getCmsRequest();
-		statusOrderCode = order.getStatusOrderCode();
-		statusOrderDescription = order.getStatusOrderDescription();
-		registerOrderDate = DateUtil.localDateTimeToString(order.getRegisterOrderDate());
-		releaseOrderDate = DateUtil.localDateTimeToString(order.getReleaseOrderDate());
-		note2 = order.getNote2();
-		idResult = order.getIdResult();
 		return this;
 	}
 
--- src/main/java/pe/telefonica/provision/service/impl/NotifyOrderServiceImpl.java
+++ src/main/java/pe/telefonica/provision/service/impl/NotifyOrderServiceImpl.java
@@ -2,10 +2,17 @@ package pe.telefonica.provision.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import pe.telefonica.provision.controller.response.ErrorResponse;
+import pe.telefonica.provision.controller.response.order.OrderNotificationResponse;
 import pe.telefonica.provision.model.order.Order;
 import pe.telefonica.provision.repository.OrderRepository;
 import pe.telefonica.provision.service.NotifyOrderService;
@@ -13,14 +20,43 @@ import pe.telefonica.provision.service.NotifyOrderService;
 @Service
 public class NotifyOrderServiceImpl implements NotifyOrderService {
 
+	private static final Log log = LogFactory.getLog(NotifyOrderServiceImpl.class);
+
 	@Autowired
 	OrderRepository orderRepository;
+
+	private MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
 
 	@Override
 	public ResponseEntity<Object> getOrders(String uServiceId, String uPid) {
 
+		headers.clear();
+		headers.add("UNICA-ServiceId", uServiceId);
+		headers.add("UNICA-PID", uPid);
+
 		List<Order> orders = orderRepository.getOrdersToNotify();
-		return null;
+
+		return evaluateOrders(orders);
+	}
+
+	private ResponseEntity<Object> evaluateOrders(List<Order> orders) {
+
+		HttpStatus status;
+		Object response;
+
+		log.info("Orders - " + (orders != null ? orders.toString() : "null"));
+
+		if (orders != null) {
+			status = HttpStatus.OK;
+			response = new OrderNotificationResponse().fromOrderList(orders);
+		} else {
+			status = HttpStatus.NOT_FOUND;
+			response = new ErrorResponse("SVC1006", String.format("Resources does not exist"),
+					"Reference to a resource identifier which does not exist in the collection/repository referred (e.g.: invalid Id)",
+					"Not existing Resource Id");
+		}
+
+		return new ResponseEntity<Object>(response, headers, status);
 	}
 
 }

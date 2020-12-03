7c5c89a50f6c23aa2abfab7a7c35a221903d958c
Augusto Alva Campos <afalva@indracompany.com>
2020-12-03 11:24:56 -0500

    Implementacion servicio notificacion de ordenes

---
+++ src/main/java/pe/telefonica/provision/service/impl/NotifyOrderServiceImpl.java
@@ -0,0 +1,17 @@
+package pe.telefonica.provision.service.impl;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import pe.telefonica.provision.service.NotifyOrderService;
+
+@Service
+public class NotifyOrderServiceImpl implements NotifyOrderService{
+
+	@Override
+	public ResponseEntity<Object> getOrders(String uServiceId, String uPid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

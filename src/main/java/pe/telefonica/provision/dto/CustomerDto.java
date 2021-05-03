03f2334d26b049b8ced8919050af00070af70826
Marcos Zorrilla <marc.zg21@gmail.com>
2021-05-03 18:12:26 -0500

    update latitud y longiud

--- src/main/java/pe/telefonica/provision/dto/CustomerDto.java
+++ src/main/java/pe/telefonica/provision/dto/CustomerDto.java
@@ -48,6 +48,22 @@ public class CustomerDto implements Serializable {
 
 	@Field("longitude")
 	private String longitude;
+	
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
 
 	public String getDocumentType() {
 		return documentType;
@@ -149,6 +165,8 @@ public class CustomerDto implements Serializable {
 		this.department = customer.getDepartment();
 		this.address = customer.getAddress();
 		this.reference = customer.getReference();
+		this.latitude = customer.getLatitude();
+		this.longitude = customer.getLongitude();
 		return this;
 	}
 }

e887a8f1443d19a601677921040c90a689c48d78
Gabioto <dafne_rossi@hotmail.com>
2021-12-13 16:59:39 -0500

    cert

--- src/main/java/pe/telefonica/provision/util/config/MongoConfig.java
+++ src/main/java/pe/telefonica/provision/util/config/MongoConfig.java
@@ -13,14 +13,14 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
 	@Override
 	protected String getDatabaseName() {
-		return System.getenv("TDP_DATA_MONGODB_DATABASE");
-		//return "compose";
+		//return System.getenv("TDP_DATA_MONGODB_DATABASE");
+		return "compose";
 	}
 
 	@Override
 	public MongoClient mongoClient() {
-		//ConnectionString connectionString = new ConnectionString("mongodb://admin:SNOKIDFOQWHKSMXO@portal-ssl1346-52.bmix-dal-yp-b97ae098-6774-4b4d-8fee-2d41318ea29e.3558158292.composedb.com:23712,portal-ssl1226-53.bmix-dal-yp-b97ae098-6774-4b4d-8fee-2d41318ea29e.3558158292.composedb.com:23712/compose?authSource=admin&ssl=true");
-		ConnectionString connectionString = new ConnectionString(System.getenv("TDP_DATA_MONGODB_URI"));
+		ConnectionString connectionString = new ConnectionString("mongodb://account-trazabilidad-provision-cert:KiJ8oByP0ZMaIglR7Kzno3z2ZzIQllKNe47eJ7Lj0NZLPvlFxUgYbOCJZxRcbPn4pBN2es5awd3Ak6XxpqVvuw==@account-trazabilidad-provision-cert.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@account-trazabilidad-provision-cert@");
+		//ConnectionString connectionString = new ConnectionString(System.getenv("TDP_DATA_MONGODB_URI"));
 		MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString)
 				.build();
 
